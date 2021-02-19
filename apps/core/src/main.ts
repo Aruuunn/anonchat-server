48098f4acc702a8b8dc9f7b34537b3382f1a8580
Arun Murugan <arunmurugan200@gmail.com>
2021-02-19 10:21:21 +0530

    Fix host name of server for hosting at heroku

--- apps/core/src/main.ts
+++ apps/core/src/main.ts
@@ -1,29 +1,33 @@
-import {NestFactory} from '@nestjs/core';
-import {FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import fastifyCookie from 'fastify-cookie';
-import {AppModule} from './modules/app.module';
-import {TransformOutputInterceptor} from './common/interceptors/transform-output.interceptor';
-import {ALLOWED_ORIGINS} from './config';
-
+import { AppModule } from './modules/app.module';
+import { TransformOutputInterceptor } from './common/interceptors/transform-output.interceptor';
+import { ALLOWED_ORIGINS } from './config';
 
 const PORT = process.env.PORT || 8000;
+const HOST = process.env.YOUR_HOST || '0.0.0.0';
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestFastifyApplication>(
-        AppModule,
-        new FastifyAdapter({
-            logger: false
-        }));
-    app.enableCors({
-        credentials: true,
-        origin: ALLOWED_ORIGINS,
-    });
-    await app.register(fastifyCookie);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: false,
+    }),
+  );
+  app.enableCors({
+    credentials: true,
+    origin: ALLOWED_ORIGINS,
+  });
+  await app.register(fastifyCookie);
 
-    app.useGlobalInterceptors(new TransformOutputInterceptor());
-    await app.listen(PORT);
+  app.useGlobalInterceptors(new TransformOutputInterceptor());
+  await app.listen(PORT, HOST);
 }
 
 bootstrap().then(() => {
-    console.log(`Server Listening at port: ${PORT}`);
+  console.log(`Server Listening at port: ${PORT}`);
 });
